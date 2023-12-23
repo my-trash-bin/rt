ba8c8160bfc7f22ce710f472f68942c6ce126da1
맹주영 <mjy9088@naver.com>
2023-12-23 16:42:32 +0900

    WIP: add bmp module

---
+++ src/bmp.rs
@@ -0,0 +1,107 @@
+use std::error::Error;
+use std::result::Result;
+
+#[derive(Debug, PartialEq)]
+pub struct MinirtBmpPixel {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct MinirtBmp {
+    width: usize,
+    height: usize,
+    extra: Vec<MinirtBmpPixel>,
+}
+
+impl MinirtBmp {
+    pub fn new(
+        width: usize,
+        height: usize,
+        fill: fn(usize, usize, &mut MinirtBmpPixel),
+    ) -> MinirtBmp {
+        let mut extra = Vec::with_capacity(width * height);
+        for y in 0..height {
+            for x in 0..width {
+                let mut pixel = MinirtBmpPixel { r: 0, g: 0, b: 0 };
+                fill(x, y, &mut pixel);
+                extra.push(pixel);
+            }
+        }
+        MinirtBmp {
+            width,
+            height,
+            extra,
+        }
+    }
+
+    pub fn serialize(&self) -> Result<Vec<u8>, Box<dyn Error>> {
+        let row_padding = (4 - (self.width * 3) % 4) % 4;
+        let row_size = self.width * 3 + row_padding;
+        let whole_size = row_size * self.height;
+        let mut result = Vec::with_capacity(54 + whole_size);
+
+        // Fill header
+        result.extend(b"BM");
+        result.extend(&(54 + whole_size as u32).to_le_bytes());
+        result.extend(&[0, 0, 0, 0]);
+        result.extend(&54u32.to_le_bytes());
+        result.extend(&40u32.to_le_bytes());
+        result.extend(&(self.width as u32).to_le_bytes());
+        result.extend(&(self.height as u32).to_le_bytes());
+        result.extend(&1u16.to_le_bytes());
+        result.extend(&24u16.to_le_bytes());
+        result.extend(&[0, 0, 0, 0]);
+        result.extend(&(whole_size as u32).to_le_bytes());
+        result.extend(&[0, 0, 0, 0]);
+        result.extend(&[0, 0, 1, 0]);
+        result.extend(&[0, 0, 0, 0]);
+        result.extend(&[0, 0, 0, 0]);
+        result.extend(&[0, 1, 0, 0]);
+        result.extend(&[0, 0, 0, 0]);
+
+        // Fill body
+        for y in (0..self.height).rev() {
+            for x in 0..self.width {
+                let pixel = &self.extra[y * self.width + x];
+                result.push(pixel.b);
+                result.push(pixel.g);
+                result.push(pixel.r);
+            }
+            for _ in 0..row_padding {
+                result.push(0);
+            }
+        }
+
+        Ok(result)
+    }
+
+    pub fn deserialize(buffer: &[u8]) -> Result<MinirtBmp, Box<dyn Error>> {
+        if buffer.len() <= 54 || &buffer[0..2] != b"BM" {
+            return Err("Invalid BMP file format".into());
+        }
+
+        let width = i32::abs(i32::from_le_bytes(buffer[18..22].try_into()?)) as usize;
+        let height = i32::abs(i32::from_le_bytes(buffer[22..26].try_into()?)) as usize;
+        let row_padding = (4 - (width * 3) % 4) % 4;
+        let row_size = width * 3 + row_padding;
+        let whole_size = row_size * height;
+
+        if buffer.len() < 54 + whole_size {
+            return Err("Invalid BMP file size".into());
+        }
+
+        let mut extra = Vec::with_capacity(width * height);
+        for y in (0..height).rev() {
+            for x in 0..width {
+                let r = buffer[54 + y * row_size + x * 3 + 2];
+                let g = buffer[54 + y * row_size + x * 3 + 1];
+                let b = buffer[54 + y * row_size + x * 3];
+                extra.push(MinirtBmpPixel { r, g, b });
+            }
+        }
+
+        Ok(MinirtBmp { width, height, extra })
+        }
+}
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod bmp;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, Read, Write};
+use std::result::Result;
+
+use bmp::MinirtBmp;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Example usage
+
+    let bmp = MinirtBmp::new(1024, 1024, |x, y, pixel| {
+        pixel.r = (x / 4) as u8;
+        pixel.g = (y / 4) as u8;
+        pixel.b = 255;
+    });
+    let serialized = bmp.serialize()?;
+    write_bmp_file("example.bmp", &serialized)?;
+
+    let buffer = read_bmp_file("example.bmp")?;
+    let bmp = MinirtBmp::deserialize(&buffer)?;
+    let serialized = bmp.serialize()?;
+    write_bmp_file("output.bmp", &serialized)?;
+    Ok(())
+}
+
+fn read_bmp_file(filename: &str) -> io::Result<Vec<u8>> {
+    let mut file = File::open(filename)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn write_bmp_file(filename: &str, data: &[u8]) -> io::Result<()> {
+    let mut file = File::create(filename)?;
+    file.write_all(data)?;
+    Ok(())
+}
